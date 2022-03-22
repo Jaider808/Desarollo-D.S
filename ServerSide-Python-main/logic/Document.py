35a0ba2569f689c9d875a7dc6f5af89a810e46e8
Jaider808 <98894637+Jaider808@users.noreply.github.com>
2022-03-22 08:03:30 -0500

    Add files via upload

---
+++ ServerSide-Python-main/logic/Document.py
@@ -0,0 +1,8 @@
+class Document:
+    def __init__(self, id_document, titulo, N_pagina, categoria, autores):
+
+        self.id_document = id_document
+        self.titulo = titulo
+        self.N_pagina = N_pagina
+        self.categoria = categoria
+        self.autores = autores
